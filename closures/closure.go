1ab02e059fdfd4a5b666b9b402365e6a684f8a57
Subair-Ahamed <155146711+Subair-Ahamed@users.noreply.github.com>
2024-01-04 10:07:01 +0530

    Update

--- closures/closure.go
+++ closures/closure.go
@@ -11,6 +11,24 @@ func sequence() func() int {
 
 }
 
+//Example-1:
+func greet() func() {
+	name := "raju"
+	return func() {
+		fmt.Println("Hi", name)
+	}
+}
+
+//nested function:
+func showName(input string) { //outer function
+
+	var display = func() { //inner function
+		fmt.Println("Hi", input)
+	}
+
+	display() //calling inner function
+}
+
 func main() {
 
 	int_seq := sequence()
@@ -19,5 +37,11 @@ func main() {
 
 	int_seq1 := sequence()
 	fmt.Println(int_seq1())
+	//example-1:
+	greeting := greet()
+	greeting()
+
+	//nested function:
+	showName("Prakash") //calling outer function
 
 }
