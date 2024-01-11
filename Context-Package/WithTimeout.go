596e261cae28c3321dc28b30bf9e5c913dfd0e53
Subair-Ahamed <155146711+Subair-Ahamed@users.noreply.github.com>
2024-01-11 15:28:12 +0530

    first commit

--- Context-Package/WithTimeout.go
+++ Context-Package/WithTimeout.go
@@ -9,7 +9,7 @@ import (
 func employee(ctx context.Context) error{
 	for{
 		select{
-        case <- ctx.Done():
+                case <- ctx.Done():
 			fmt.Println("Error occured")
 			return ctx.Err() //to get the reason for context cancellation.
 
@@ -17,18 +17,18 @@ func employee(ctx context.Context) error{
 			fmt.Println("Operation is running..!")
 			return nil
 			time.After(2 * time.Second)
-		}
-	}
+		      }
+	   }
 }
 
 func main(){
-	ctx,cancel := context.WithTimeout(context.Background(), 5 * time.Second) //same like WithCancel() but also takes duration
+	ctx,cancel := context.WithTimeout(context.Background(), 5 * time.Second)   //same like WithCancel() but also takes duration
 	
 	defer cancel()
 
 	err := go employee(ctx)
 
-	if err != nil{
+	if err != nil {
 		fmt.Println("Main: Operation failed..!",err)
 	}
 
